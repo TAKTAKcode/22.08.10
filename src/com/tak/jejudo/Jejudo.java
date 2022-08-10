f9478ee7749a2be46a86426b200cfa994c8e68bf
tak <wontaek22@gmail.com>
2022-08-10 22:41:23 +0900

    22.08.10
    Jejudo..2

---
+++ src/com/tak/jejudo/Jejudo.java
@@ -0,0 +1,9 @@
+package com.tak.jejudo;
+
+public class Jejudo {
+
+    public static void main(String[] args) {
+
+        System.out.println("제주도 2일차.. 예상보다 너무 더워서 쓰러지기 직전이다.. 비는 안와서 좋지만.. 전기통닭이 이런 기분일까. 한번 깊게 생각을 하게 된다.");
+    }
+}
